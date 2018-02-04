4f237bc007fe74c49e5df348dffaa7860bce41e7
Micah Miller-Eshleman <micah.millereshleman@gmail.com>
2018-02-04 02:32:53 -0800

    Automatically handle auth tokens after redirect

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function() {
 
-  window.webAuth = new auth0.WebAuth({
+  // Accessible globally as auth0.webAuth
+  var webAuth = new auth0.WebAuth({
     domain: 'any-mammals-milk.auth0.com',
     clientID: '8kL5tAS66BwF7gphZQURQwwGR720ofy5',
     responseType: 'token id_token',
@@ -9,6 +10,7 @@ window.addEventListener('load', function() {
     redirectUri: window.location.href
   });
 
+  // Login when user clicks button
   var loginBtn = document.getElementById('btn-login');
 
   loginBtn.addEventListener('click', function(e) {
@@ -16,31 +18,40 @@ window.addEventListener('load', function() {
     webAuth.authorize();
   });
 
+  // After login redirects user to page, remove hash and connect to API 
+  // to get user data
+  handleAuthentication();
+
+  // for debugging
+  window.handleAuthentication = handleAuthentication;
+
+  function handleAuthentication() {
+    webAuth.parseHash(function(err, authResult) {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        window.location.hash = '';
+        setSession(authResult);
+        console.log('user successfully logged in!');
+      } else if (err) {
+        console.log(err);
+        alert('Error: ' + err.error + '. Check the console for further details.');
+      }
+      // displayButtons();
+    });
+  }
+
+  function setSession(authResult) {
+    // Set the time that the access token will expire at
+    var expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+      );
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  }
+
 }); 
 
-function handleAuthentication() {
-  webAuth.parseHash(function(err, authResult) {
-    if (authResult && authResult.accessToken && authResult.idToken) {
-      window.location.hash = '';
-      setSession(authResult);
-      console.log('user successfully logged in!');
-    } else if (err) {
-      console.log(err);
-      alert('Error: ' + err.error + '. Check the console for further details.');
-    }
-    // displayButtons();
-  });
-}
 
-function setSession(authResult) {
-  // Set the time that the access token will expire at
-  var expiresAt = JSON.stringify(
-    authResult.expiresIn * 1000 + new Date().getTime()
-    );
-  localStorage.setItem('access_token', authResult.accessToken);
-  localStorage.setItem('id_token', authResult.idToken);
-  localStorage.setItem('expires_at', expiresAt);
-}
 
 
   //------------------------
