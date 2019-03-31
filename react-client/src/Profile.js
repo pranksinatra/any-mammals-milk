17f45eb26d1ba45e0e94658a5b80931c403bfb25
Micah Miller-Eshleman <micah.millereshleman@gmail.com>
2019-03-30 17:56:57 -0700

    Add link to Intern Application

--- react-client/src/Profile.js
+++ react-client/src/Profile.js
@@ -6,7 +6,7 @@ import Preferences from './Preferences';
 const Block = styled('div')`
   margin: 0 auto 1rem;
   padding: 1rem;
-  width: 40rem;
+  width: 30rem;
   max-width: 100%;
 `;
 
@@ -18,7 +18,7 @@ const Button = styled('button')`
 export default function Profile({ user }) {
   const firstName = user.displayName.state.split(' ')[0];
 
-  const heading = user.isAnonymous ? `Hey there!` : `Hey ${firstName}!`;
+  const heading = user.isAnonymous ? `Hey there!` : `Hey ${firstName},`;
 
   return (
     <main>
@@ -26,6 +26,21 @@ export default function Profile({ user }) {
       <Block>
         <p style={{ textAlign: 'center', marginTop: 0 }}>
           Thanks for stopping by.
+        </p>
+        <p style={{ textAlign: 'center', marginTop: 0 }}>
+          We're building a modern milk marketplace to better represent the
+          diversity of milk producers in our community.
+        </p>
+        <p style={{ textAlign: 'center', marginTop: 0 }}>
+          Want to get involved? Consider{' '}
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSeFkQEd9EW5CJJPDxsa8Liqi9MJXo7836Io32xYq1un1l7ndA/viewform?usp=sf_link"
+            target="_blank"
+            rel="noopener"
+          >
+            becoming an intern
+          </a>
+          .
         </p>
         <Preferences user={user} isCentered={true} />
         {!user.isAnonymous ? (
